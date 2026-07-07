3f95dae0c6a49eec05e0ed208b4f6afd5a0449ef
Gangaraju nagam <nagamraju457@gmail.com>
2026-07-07 16:59:28 +0530

    first commit of stdmanagement sys

---
+++ src/main/java/com/example/demo/stdController.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class stdController {
+	
+	@Autowired
+	private studentRepo repo;
+	@GetMapping("/")
+	public String home()
+	{
+		return"home";
+	}
+	@GetMapping("/insert")
+	public String insertPage()
+	{
+		return"insert";
+	}
+	@PostMapping("/insert")
+	public String insert(@ModelAttribute Student std)
+	{
+		repo.save(std);
+		return"redirect:/read";
+	}
+	@GetMapping("/read")
+	public String read(Model model)
+	{
+		model.addAttribute("students",repo.findAll());
+		return "read";
+	}
+	@GetMapping("/update")
+	public String update()
+	{
+		return "update";
+	}
+	@PostMapping("/update")
+	public String updatePage(@ModelAttribute Student std)
+	{
+		repo.save(std);
+		return "redirect:/read";
+	}
+	@GetMapping("/delete")
+	public String deletePage()
+	{
+		return "delete";
+	}
+	@PostMapping("/delete")
+	public String delete(@ModelAttribute Student std)
+	{
+		repo.deleteById(std.getId());
+		return "redirect:/read";
+	}
+	
+
+}
